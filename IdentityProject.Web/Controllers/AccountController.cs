8a6efd8a644bcd65753ef9ef4f023c946e60aded
JORGE ALEXANDER ARDILA RESTREPO <jorge.ardila1641@correo.policia.gov.co>
2024-01-04 23:06:08 -0500

    i created register post method with AntiForgeryToken

--- IdentityProject.Web/Controllers/AccountController.cs
+++ IdentityProject.Web/Controllers/AccountController.cs
@@ -1,20 +1,19 @@
+using IdentityProject.Domain.Entities;
 using IdentityProject.Web.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace IdentityProject.Web.Controllers
 {
     public class AccountController : Controller
     {
-        private readonly ILogger<AccountController> _logger;
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
 
-        public AccountController(ILogger<AccountController> logger)
+        public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
         {
-            _logger = logger;
-        }
-
-        public IActionResult Index()
-        {
-            return View();
+            _userManager = userManager;
+            _signInManager = signInManager;
         }
 
         [HttpGet]
@@ -22,6 +21,48 @@ namespace IdentityProject.Web.Controllers
         {
             RegisterViewModel model = new();
             return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new AppUser
+                {
+                    UserName = model.UserName,
+                    Email = model.Email,
+                    Name = model.Name,
+                    Address = model.Address,
+                    Birthdate = model.Birthdate,
+                    Country = model.Country,
+                    CountryCode = model.CountryCode,
+                    City = model.City,
+                    Url = model.Url,
+                    PhoneNumber = model.PhoneNumber
+                };
+
+                var result = await _userManager.CreateAsync(user, model.Password);
+
+                if (result.Succeeded)
+                {
+                    await _signInManager.SignInAsync(user, isPersistent: false);
+                    return RedirectToAction(nameof(HomeController.Index), "Home");
+                }
+
+                ValidateErrors(result);
+            }
+
+            return View(model);
+        }
+
+        private void ValidateErrors(IdentityResult result)
+        {
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError(string.Empty, error.Description);
+            }
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
--- IdentityProject.Web/Models/RegisterViewModel.cs
+++ IdentityProject.Web/Models/RegisterViewModel.cs
@@ -4,18 +4,20 @@ namespace IdentityProject.Web.Models
 {
     public class RegisterViewModel
     {
-        [Required(ErrorMessage = "El nombre de usuario es obligatorio")]
-        [StringLength(20, ErrorMessage = "El {0} debe tener al menos {2} caracteres y máximo {1}", MinimumLength = 5)]
+        [Required(ErrorMessage = "El {0} es obligatorio")]
+        [StringLength(20, ErrorMessage = "El {0} debe tener entre {2} y {1} caracteres.", MinimumLength = 5)]
+        [DataType(DataType.Text)]        
         [Display(Name = "Usuario")]
         public string UserName { get; set; } = string.Empty;
 
-        [Required(ErrorMessage = "El Email es obligatorio")]
+        [Required(ErrorMessage = "El {0} es obligatorio")]
         [EmailAddress]
         [Display(Name = "Correo electrónico")]
+        [DataType(DataType.EmailAddress)]
         public string Email { get; set; } = string.Empty;
 
         [Required(ErrorMessage = "La contraseña es obligatoria")]
-        [StringLength(50, ErrorMessage = "La {0} debe tener al menos {2} caracteres y máximo {1}", MinimumLength = 6)]
+        [StringLength(50, ErrorMessage = "La {0} debe tener entre {2} y {1} caracteres.", MinimumLength = 6)]
         [DataType(DataType.Password)]
         [Display(Name = "Contraseña")]
         public string Password { get; set; } = string.Empty;
@@ -26,31 +28,46 @@ namespace IdentityProject.Web.Models
         [Display(Name = "Confirmar Contraseña")]
         public string ConfirmPassword { get; set; } = string.Empty;
 
-        [Required(ErrorMessage = "El nombre es obligatorio")]
+        [Required(ErrorMessage = "El {0} es obligatorio")]
+        [StringLength(50, ErrorMessage = "El {0} debe tener entre {2} y {1} caracteres.", MinimumLength = 7)]
+        [DataType(DataType.Text)]
         [Display(Name = "Nombre")]
         public string Name { get; set; } = string.Empty;
 
+        [StringLength(40, ErrorMessage = "La {0} debe tener entre {2} y {1} caracteres.", MinimumLength = 5)]
+        [DataType(DataType.Url)]      
         [Display(Name = "Página web")]
         public string Url { get; set; } = string.Empty;
 
+        [Range(1, 999)]
         [Display(Name = "Código país")]
         public int CountryCode { get; set; }
 
+        [StringLength(20, ErrorMessage = "El {0} debe tener entre {2} y {1} caracteres.", MinimumLength = 5)]
+        [DataType(DataType.PhoneNumber)]
         [Display(Name = "Teléfono")]
         public string PhoneNumber { get; set; } = string.Empty;
 
-        [Required(ErrorMessage = "El país es obligatorio")]
+        [Required(ErrorMessage = "El {0} es obligatorio")]
+        [StringLength(20, ErrorMessage = "El {0} debe tener entre {2} y {1} caracteres.", MinimumLength = 3)]
+        [DataType(DataType.Text)]
         [Display(Name = "País")]
         public string Country { get; set; } = string.Empty;
 
+        [StringLength(30, ErrorMessage = "La {0} debe tener entre {2} y {1} caracteres.", MinimumLength = 3)]
+        [DataType(DataType.Text)]
         [Display(Name = "Ciudad")]
         public string City { get; set; } = string.Empty;
 
+        [StringLength(40, ErrorMessage = "La {0} debe tener entre {2} y {1} caracteres.", MinimumLength = 5)]
+        [DataType(DataType.Text)]
         [Display(Name = "Dirección")]
         public string Address { get; set; } = string.Empty;
 
-        [Required(ErrorMessage = "La Fecha de Nacimiento es obligatoria")]
+        [Required(ErrorMessage = "La {0} es obligatoria")]
         [Display(Name = "Fecha de Nacimiento")]
+        [DataType(DataType.Date)]
         public DateTime Birthdate { get; set; }
+        
     }
 }
